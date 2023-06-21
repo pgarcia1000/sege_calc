3681a1920b05943f2088ed38fe7b1277e369aa96
Agustin <alomeli@cpte.gob.mx>
2023-06-21 11:46:10 -0600

    cambio4

--- potencia.java
+++ potencia.java
@@ -8,7 +8,7 @@ class potencia {
 
         for (int i = 1; i <= y; i++) {
 
-            resultado = resultado * x -5;
+            resultado = resultado * x -5+4;
 
         }
         System.out.println(resultado);
