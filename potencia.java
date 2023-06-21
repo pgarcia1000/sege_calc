12011e4cfb0e52d057cdc827126e6f9667b45b9e
YohnathanAS <yaronia@cpte.gob.mx>
2023-06-21 11:31:05 -0600

    Calcular potencia de la base

---
+++ potencia.java
@@ -0,0 +1,32 @@
+class potencia {
+
+    public static void main(String[] args) {
+        // int a = Integer.parseInt(arg[0]);
+        int x = 3;
+        int y = 3;
+        int resultado = 1;
+
+        for (int i = 1; i <= y; i++) {
+
+            resultado = resultado * x;
+
+        }
+        System.out.println(resultado);
+
+    }
+    /*
+     * public static int Numero_elevado(int x, int z) {
+     * 
+     * int resultado = 1;
+     * 
+     * for (int i = 1; i <= z; i++) {
+     * 
+     * resultado = resultado * x;
+     * 
+     * }
+     * 
+     * return resultado;
+     * 
+     * }
+     */
+}
