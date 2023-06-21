d77c78faacdde3f7dc27a7bc9330c04c48fe3db9
Agustin <alomeli@cpte.gob.mx>
2023-06-21 11:31:16 -0600

    ya tiene multiplicacion

--- Casio.java
+++ Casio.java
@@ -3,7 +3,11 @@ class Casio{
     public static void main(String[] arg){
         int a=Integer.parseInt(arg[0]);
         int b=Integer.parseInt(arg[1]);
-        System.out.println("La suma es: "+(a+b));
+        //System.out.println("La suma es: "+(a+b));
+
+        System.out.println("La MULTIPLICACION es: "+(a*b));
+
+
         
     }
 }
